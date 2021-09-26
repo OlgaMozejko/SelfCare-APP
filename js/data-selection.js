c04d7cb079e2cc77ea3862f132802a0dc1081af6
OlgaMozejko <78202579+OlgaMozejko@users.noreply.github.com>
2021-09-26 09:02:49 +0200

    om
    working fetch

--- js/data-selection.js
+++ js/data-selection.js
@@ -62,67 +62,48 @@ class MoodSelector {
     }
     document.querySelector('#activities-container').innerHTML = htmlTemplate;
   }
-/*
+
   appendCategories() {
     let htmlTemplate = "";
     for (let category of this.categories) {
-      htmlTemplate += 
+      htmlTemplate += `
         <option value="${category.id}">${category.name}</option>
       `;
     }
 
-    document.querySelector('#select-category').innerHTML += htmlTemplate;
-  } */
+    //document.querySelector('#select-category').innerHTML += htmlTemplate;
+  } 
 
-  appendPostsByCategory(movies) {
+  appendTags() {
     let htmlTemplate = "";
-    for (let movie of movies) {
+    for (let tag of this.tags) {
+      htmlTemplate += `
+        <option value="${tag.id}">${tag.name}</option>
+      `;
+    }
+    //document.querySelector('#activities-container').innerHTML = htmlTemplate;
+  }
+
+  appendPostsByCategory(posts) {
+    let htmlTemplate = "";
+    for (let post of posts) {
       htmlTemplate += /*html*/ `
-        <article onclick="showDetailView('${movie.id}')">
-            <h2>${movie.title.rendered} (${movie.acf.year})</h2>
-            <img src="${this.getFeaturedImageUrl(movie)}">
-            <p>${movie.excerpt.rendered}</p>
-        </article>
+      <article>
+      <h2>${post.title.rendered}</h2>
+      <p>${post.acf.description}</p>
+      </article>
     `;
     }
     // if no movies, display feedback to the user
     if (movies.length === 0) {
       htmlTemplate = /*html*/`
-        <p>No Movies</p>
+        <p>No Activities</p>
       `;
     }
 
-    document.querySelector('#movies-by-category-container').innerHTML = htmlTemplate;
+    //document.querySelector('#movies-by-category-container').innerHTML = htmlTemplate;
   }
 
-  getFeaturedImageUrl(post) {
-    let imageUrl = "";
-    if (post._embedded['wp:featuredmedia']) {
-      imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
-    }
-    return imageUrl;
-  }
-
-  search(value) {
-    let searchValue = value.toLowerCase();
-    let filteredMovies = this.movies.filter(movie => movie.title.rendered.toLowerCase().includes(searchValue));
-    this.appendMovies(filteredMovies);
-  }
-
-  showDetailView(id) {
-    const movie = this.movies.find(movie => movie.id == id);
-    document.querySelector("#detailView h2").innerHTML = movie.title.rendered;
-    document.querySelector("#detailViewContainer").innerHTML = /*html*/`
-        <img src="${this.getFeaturedImageUrl(movie)}">
-        <article>
-            <h1>${movie.title.rendered}</h1>
-            <h2>${movie.acf.year}</h2>
-            <p>${movie.content.rendered}</p>
-            <iframe src="${movie.acf.trailer}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        </article>
-    `;
-    location.href = "#detailView";
-  }
 
 }
 
