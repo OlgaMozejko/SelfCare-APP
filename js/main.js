3618d36c70f2ada413d433535625ef77c35ddbbb
OlgaMozejko <78202579+OlgaMozejko@users.noreply.github.com>
2021-09-27 21:28:35 +0200

    om
    append by different emotions from home - done
    append by different time at the same time - done
    changing bobs on time selection
    etc

--- js/main.js
+++ js/main.js
@@ -19,13 +19,9 @@ import MoodSelector from "./data-selection.js";
 let moodSelector = new MoodSelector();
 moodSelector.init();
 
-//------------ Olga -----------
-//picking category based on the face selected by user &
-//change of text & background color :  based on the selected mood
-//picking tag based on tag in a drop-down selection
-
 let html = "";
 let _selectedCatId;
+let bob = "";
 
 window.filterbyEmotions = function (value) {
   moodSelector.filterByEmotions(value);
@@ -46,7 +42,18 @@ window.orderByOldest = function () {
   moodSelector.orderByOldest();
 }
 
+//------------ Olga -----------
+//picking category based on the face selected by user &
+//change of text & background color :  based on the selected mood
+//picking tag based on tag in a drop-down selection
+
+window.clearHtml = function () {
+  let oldContent = document.querySelector('#activities-container');
+  oldContent.innerHTML = "";
+}
+
 function categorySelected(catId) {
+  clearHtml();
   _selectedCatId = catId;
   if (catId === "2") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -54,6 +61,9 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--happy-yellow)";
+    document.querySelector("#armyofBobs").innerHTML = "";
+    bob = `<img src="img/happyhappyboi.png">`;
+    document.querySelector("#armyofBobs").innerHTML = bob;
   }
   if (catId === "6") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -61,6 +71,9 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--indiffrent-brown)";
+      document.querySelector("#armyofBobs").innerHTML = "";
+      bob = `<img src="img/Indifferentboi.png">`;
+      document.querySelector("#armyofBobs").innerHTML = bob;
   }
   if (catId === "3") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -68,6 +81,9 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--sad-blue)";
+      document.querySelector("#armyofBobs").innerHTML = "";
+      bob = `<img src="img/sadboi.png" class="blueboi">`;
+      document.querySelector("#armyofBobs").innerHTML = bob;
   }
   if (catId === "5") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -75,6 +91,9 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--unmotivated-green)";
+      document.querySelector("#armyofBobs").innerHTML = "";
+      bob = `<img src="img/unmotivatedboi.png">`;
+      document.querySelector("#armyofBobs").innerHTML = bob;
   }
   if (catId === "8") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -83,6 +102,9 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--tired-purple)";
+      document.querySelector("#armyofBobs").innerHTML = "";
+      bob = `<img src="img/sleepysleepyboi.png" class="sleepyboi">`;
+      document.querySelector("#armyofBobs").innerHTML = bob;
   }
   if (catId === "7") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -91,6 +113,9 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--stressed-grey)";
+      document.querySelector("#armyofBobs").innerHTML = "";
+      bob = `<img src="img/stressedboi.png" class="blueboi">`;
+      document.querySelector("#armyofBobs").innerHTML = bob;
   }
   if (catId === "4") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -98,6 +123,9 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--frustrated-red";
+      document.querySelector("#armyofBobs").innerHTML = "";
+      bob = `<img src="img/angryboi.png">`;
+      document.querySelector("#armyofBobs").innerHTML = bob;
   }
   if (catId === "9") {
     document.querySelector("#changing-text").innerHTML = "";
@@ -106,26 +134,16 @@ function categorySelected(catId) {
     document.querySelector("#changing-text").innerHTML += html;
     document.querySelector("#time-selection-wrapper").style.backgroundColor =
       "var(--anxious-purple)";
+      document.querySelector("#armyofBobs").innerHTML = "";
+      bob = `<img src="img/anxiousboi.png" class="purpleboi">`;
+      document.querySelector("#armyofBobs").innerHTML = bob;
   }
 }
 
 function timeSelected(tagId) {
-  if (tagId === "10") {
-    moodSelector.getPostsByCatAndTag(_selectedCatId, tagId);
-    navigateTo("#/allActivities");
-  }
-  if (tagId === "12") {
-    moodSelector.getPostsByCatAndTag(_selectedCatId, tagId);
-    navigateTo("#/allActivities");
-  }
-  if (tagId === "11") {
-    moodSelector.getPostsByCatAndTag(_selectedCatId, tagId);
-    navigateTo("#/allActivities");
-  }
-  if (tagId === "13") {
-    moodSelector.getPostsByCatAndTag(_selectedCatId, tagId);
-    navigateTo("#/allActivities");
-  }
+  if (tagId === "10" || tagId === "11" || tagId === "12" || tagId === "13"){
+  moodSelector.getPostsByCatAndTag(_selectedCatId, tagId);
+  navigateTo("#/allActivities");}
 }
 
 window.categorySelected = (catId) => categorySelected(catId);
